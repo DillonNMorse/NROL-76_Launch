fca66cf390bb197bd6d23069a2648de093a148ad
Dillon Morse <DillonNMorse@gmail.com>
2020-07-17 09:54:53 -0600

    Comment code

--- Scrape_Data.py
+++ Scrape_Data.py
@@ -11,6 +11,9 @@ from keras.models import load_model
 import time
 
 
+# =============================================================================
+# Create list of strings with appropriate frame-numbers for accessing image files
+# =============================================================================
 frame_start = 193
 frame_end = 13993
 Frames = [str(i) for i in np.arange(frame_start, frame_end, 6) ]
@@ -20,25 +23,46 @@ for j, frame in enumerate(Frames):
     elif len(frame) == 4:
         Frames[j] = '0'+frame
 
+
+# =============================================================================
+# Create pandas dataframe to hold scraped data
+# =============================================================================
 df = ( pd.DataFrame(Frames)
       .rename( columns = {0:'Frame'}))
 df['Alt'] = np.zeros(len(Frames))
 df['Speed'] = np.zeros(len(Frames))
 
+
+# =============================================================================
+# Loop over all 2,301 frames
+# =============================================================================
 kk = 0
 start = time.time()
+model = load_model("test_model.h5")
 for frame in Frames:
-#    for k in np.arange(7): # Number of digits available on each video frame
-    model = load_model("test_model.h5")
-    (speed_ones_digit, speed_tens_digit, speed_hunds_digit,
-     speed_thous_digit, dist_ones_digit, dist_tens_digit, 
-     dist_hunds_digit ) = Read_Digit(frame, model)
-        
+
+    # =========================================================================
+    # Call Read_Digit to extract each of the 7 digits available on the frame
+    # =========================================================================
+    (speed_ones_digit,  speed_tens_digit, 
+     speed_hunds_digit, speed_thous_digit,
+      dist_ones_digit,   dist_tens_digit, 
+      dist_hunds_digit )                   = Read_Digit(frame, model)
+    
+    
+    # =========================================================================
+    # Compute the rocket speed based on the scraped digits
+    # ========================================================================= 
     speed = (  speed_ones_digit
              + speed_tens_digit*10
              + speed_hunds_digit*100
              + speed_thous_digit*1000)
     
+    # =========================================================================
+    # Compute rocket altitude based on scraped digits. Need to account for the
+    # change in formatting of on-screen numbers as altitude exceeds 100 km (
+    # which is true between frames 4687 and 10555).
+    # =========================================================================
     if (int(frame) < 4687 ) | (int(frame) > 10555 ):
         altitude = (  dist_ones_digit*0.1 
                     + dist_tens_digit 
@@ -48,14 +72,19 @@ for frame in Frames:
                     + dist_tens_digit*10
                     + dist_hunds_digit*100)        
         
-    
+    # =========================================================================
+    # Assign values to dataframe, print encouraging numbers for runtime
+    # =========================================================================
     df.loc[kk, 'Speed'] = speed
     df.loc[kk, 'Alt'] = altitude
     kk += 1
     end = time.time()
-    print('You are {:.2f}% done after {:.1f} seconds'.format(kk/len(Frames)*100, end-start))
+    print('You are {:.2f}% done after {:.1f} seconds'
+          .format(kk/len(Frames)*100, end-start))
     print('Speed is: ', speed, 'm/s')
     print('Altitude is: ', altitude, 'km')
     
-
+# =============================================================================
+# Save to csv file
+# =============================================================================
 df.to_csv('Telemtry_Data.csv')
