344942d94731b7a2a598b8b73dbe326e00ba8de5
Dillon Morse <DillonNMorse@gmail.com>
2020-07-17 10:11:25 -0600

    Comment and clean

--- Read_Frame_Digits.py
+++ Read_Frame_Digits.py
@@ -6,26 +6,34 @@ Created on Thu Jul 16 13:32:33 2020
 """
 
 from Digit_Retrieve import Retrieve_Digit
-from keras.models import load_model
-
-#frame = '01891'
+import keras
 
 
+# =============================================================================
+# Wrapper function which is called to scrape data from a frame.
+# Input: The frame number (a string, five digits long) and the prediction model
+# Output: The 7 digits necessary to read altitude and speed of the rocket
+# =============================================================================
 def Read_Digit(Frame_num, model):
     
+    # =========================================================================
+    # Call Retrieve_Digit to get the image data for the 7 digits on the frame
+    # =========================================================================
     Image = Retrieve_Digit( Frame_num )
+    
+    # =========================================================================
+    # Reshape image arrays to work with the trained classifier
+    # =========================================================================
     Images = []
     img_rows = 28
     img_cols = 22
-    for image in Image:
+    for i, image in enumerate(Image):
         x = image.reshape(1, img_rows, img_cols, 1)
         Images.append(x)
     
-    
-# =============================================================================
-#     model = load_model("test_model.h5")
-# =============================================================================
-
+    # =========================================================================
+    # Pass each image to the classifier
+    # =========================================================================
     speed_ones_digit  =  model.predict( Images[0] ).argmax()
     speed_tens_digit  =  model.predict( Images[1] ).argmax()
     speed_hunds_digit =  model.predict( Images[2] ).argmax()
@@ -34,18 +42,30 @@ def Read_Digit(Frame_num, model):
     dist_tens_digit   =  model.predict( Images[5] ).argmax()
     dist_hunds_digit  =  model.predict( Images[6] ).argmax()        
         
+    # =========================================================================
+    # Clear memory - improves runtime
+    # =========================================================================  
+    keras.backend.clear_session()
     
-
     return (speed_ones_digit, speed_tens_digit, speed_hunds_digit, speed_thous_digit,
             dist_ones_digit, dist_tens_digit, dist_hunds_digit )
 
   
  
-#print( Read_Digit( '01891' ) )
-
-
 # =============================================================================
-# import matplotlib.pyplot as plt
+# To test the functionality of this function.
+# =============================================================================
+# from keras.models import load_model
+# model = load_model("test_model.h5")
 # 
-# plt.imshow( Image[6] )
+# frame = '01891'
+# 
+# import time
+# begin = time.time()
+# 
+# print( Read_Digit(frame, model) )
+# 
+# end = time.time()
+# 
+# print('This operation took {:.3f} seconds'.format(end-begin))
 # =============================================================================
