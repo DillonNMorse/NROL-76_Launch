e27b319baacd8dd34bc8b534be237de30a7b6184
Dillon Morse <DillonNMorse@gmail.com>
2020-07-17 11:11:54 -0600

    Comment

--- Create_Labeled_Data.py
+++ Create_Labeled_Data.py
@@ -11,6 +11,10 @@ from Digit_Retrieve import Retrieve_Digit
 import matplotlib.pyplot as plt
 import pickle
 
+
+# =============================================================================
+# Create list of frame numbers as strings to retrieve image files
+# =============================================================================
 frame_start = 193
 frame_end = 13993
 Frames = [str(i) for i in np.arange(frame_start, frame_end, 6) ]
@@ -20,13 +24,17 @@ for j, frame in enumerate(Frames):
     elif len(frame) == 4:
         Frames[j] = '0'+frame
 
-
+# =============================================================================
+# Set number of frames which will be labelled. Randomly select that amount.
+# =============================================================================
 num_to_label = 30
 sub_Frames = random.choices(Frames, k = num_to_label)
 
+# =============================================================================
+# Iterate through the randomly selected frames, prompt user for labels.
+# =============================================================================
 X = []
 y = []
-
 kk = 0
 for frame in sub_Frames:
     pict_list = Retrieve_Digit(frame)
@@ -41,7 +49,9 @@ for frame in sub_Frames:
     print('You are {:.2f}% done!'.format(kk/num_to_label*100))
 Labeled_data = X, y
         
-
+# =============================================================================
+# Print label data for use as training data in Digit_Reader
+# =============================================================================
 filename = 'Labeled_Digits'
 outfile = open(filename, 'wb')
 pickle.dump(Labeled_data, outfile)
