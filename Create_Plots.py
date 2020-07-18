e8b3a96d61d2c42372095ba74038f594068f03d1
Dillon Morse <DillonNMorse@gmail.com>
2020-07-17 21:11:42 -0600

    Comment

--- Create_Plots.py
+++ Create_Plots.py
@@ -10,45 +10,62 @@ import matplotlib.pyplot as plt
 import numpy as np
 import findiff as fd
 from scipy.signal import savgol_filter
-from scipy.integrate import DOP853
 
+# =============================================================================
+# Read data, convert speed to km/s
+# =============================================================================
 telemetry = pd.read_csv('Telemetry_Data.csv').drop(columns = 'Unnamed: 0')
 telemetry['Speed'] = telemetry['Speed']/1000                            # In km/s
 
-
-k = 2301/(9*60 + 3) #frames/second (assuming video ran at constant speed)
-                    # Multiply every d/df deriv by k to account for this
-                    # (chain rule)
-  
-d_df   = fd.FinDiff(0, 1, acc = 8)
+# =============================================================================
+# Define conversion between frames and seconds, useful for computing
+# derivatives as well as for defining the time coordinate
+# =============================================================================
+k = 2301/(9*60 + 3) 
+                  
+# =============================================================================
+#   Define operators for first and second derivatives
+# =============================================================================
+d_df   = fd.FinDiff(0, 1, acc = 8) # axis zero, first derivative, accuracy = 8
 d2_df2 = fd.FinDiff(0, 1, 2, acc = 8)                   
   
+# =============================================================================
+# Smooth the scraped data, replace every point with the polynomial fit to all
+# data in a window centered at that point
+# =============================================================================
 window_size = 101
 poly_order = 1
                     
 Alt   = savgol_filter(telemetry['Alt'],   window_size, poly_order)      # In km
 Speed = savgol_filter(telemetry['Speed'], window_size, poly_order)      # In km/s
 
-
+# =============================================================================
+# Compute both components of speed vector, then smooth.
+# =============================================================================
 dAlt_dt = d_df( Alt )*k                                                 # In km/s
 dX_dt   = np.sqrt( abs(Speed**2 - (dAlt_dt)**2) )                       # In km/s
 
 dAlt_dt   = savgol_filter(dAlt_dt,   window_size, poly_order) 
 dX_dt     = savgol_filter(dX_dt,     window_size, poly_order)
 
-
-#dSpeed_dt = d_df( Speed )*k                                             # In m/s/s
-d2Alt_dt2 = d2_df2( Alt )*(k **2)*1000                                      # In km/s/s
-#dX2_dt2 = np.sqrt( abs( (dSpeed_dt)**2 - (d2Alt_dt2)**2 ) )             # In m/s/s
-dX2_dt2 = d_df( dX_dt )*1000
+# =============================================================================
+# Compute second derivatives, then smooth
+# =============================================================================
+d2Alt_dt2 = d2_df2( Alt )*(k **2)*1000                                  # In m/s/s
+dX2_dt2 = d_df( dX_dt )*1000                                            # In m/s/s
        
 d2Alt_dt2 = savgol_filter(d2Alt_dt2, window_size*2+1, poly_order) 
 dX2_dt2   = savgol_filter(dX2_dt2,   window_size*2+1, poly_order)
 
-
-
+# =============================================================================
+# Define time coordinate
+# =============================================================================
 Time_coord = np.arange(0, len(telemetry.index)*(1/k), (1/k))
 
+
+# =============================================================================
+# Make plot
+# =============================================================================
 fig, ( (ax1, ax2, ax5), (ax3, ax4, ax6) ) = plt.subplots(2,3)
 ax1.plot(Time_coord, Speed )
 ax1.set_ylabel('Total Speed (km/s)')
